0eb96b4d18da00123b64ca7ccb5dc173de48d881
trangeorge <trangeorge8@aol.com>
2023-06-15 16:36:34 -0500

    Back to the coding grind

---
+++ Leetcode/LeetCode_W3.js
@@ -0,0 +1,70 @@
+// 1337. The K weakest Rows in a Matrix
+
+var kWeakestRows = function (mat, k) {
+    var arr = []
+
+    for (var i = 0; i < mat.length; i++) {
+        var sum = 0
+        for (var j = 0; j < mat[i].length; j++) {
+            sum = sum + mat[i][j]
+        }
+        arr.push({
+            index: i,
+            count: sum,
+        })
+    }
+    return arr.sort((a, b) => a.count - b.count).slice(0, k).map(item => item.index)
+};
+
+console.log(kWeakestRows([[1, 1, 0, 0, 0],
+[1, 1, 1, 1, 0],
+[1, 0, 0, 0, 0],
+[1, 1, 0, 0, 0],
+[1, 1, 1, 1, 1]], 3))
+
+
+console.log(kWeakestRows([[1, 0, 0, 0],
+[1, 1, 1, 1],
+[1, 0, 0, 0],
+[1, 0, 0, 0]], 2))
+
+
+// 1446. Consecutive Characters
+
+var maxPower = function (s) {
+
+    var counter = 1
+    var max = 1
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] === s[i + 1]) {
+            counter++
+            max = Math.max(counter, max)
+        } else {
+            counter = 1
+        }
+    } return max
+}
+
+// part 2
+
+var maxPower2 = function (s) {
+    var arr = s.split("")
+    var counter = 0
+    var max = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i + 1]) {
+            counter++
+        } else {
+            counter++
+            if (max < counter) {
+                max = counter
+            }
+            counter = 0
+        }
+    } return max
+};
+
+console.log(maxPower2("leetcode"))
+console.log(maxPower2("leeeeetcodeee"))
