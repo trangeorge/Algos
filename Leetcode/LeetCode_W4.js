21b9e6a890af033322018a40bfaac11d7b1dc3bd
trangeorge <trangeorge8@aol.com>
2023-06-24 21:42:48 -0500

    updated 6/24

--- Leetcode/LeetCode_W4.js
+++ Leetcode/LeetCode_W4.js
@@ -141,3 +141,19 @@ var findNonMinOrMax = function (nums) {
 console.log(findNonMinOrMax([3, 2, 1, 4]))
 console.log(findNonMinOrMax([1, 2]))
 console.log(findNonMinOrMax([2, 1, 3]))
+
+// 2057 Smallest Index with Equal Value
+
+var smallestEqual = function (nums) {
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 10 === nums[i]) {
+            return i
+        }
+    }
+    return -1;
+};
+
+console.log(smallestEqual([0, 1, 2]))
+console.log(smallestEqual([4, 3, 2, 1]))
+console.log(smallestEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
