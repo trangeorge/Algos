1baf650a948b15da6179c219c11571eff17958b7
trangeorge <trangeorge8@aol.com>
2023-06-27 15:23:27 -0500

    Updated 6/27

--- Leetcode/LeetCode_W5.js
+++ Leetcode/LeetCode_W5.js
@@ -14,3 +14,29 @@ var findErrorNums = function (nums) {
 
 console.log(findErrorNums([1, 2, 2, 4]))
 console.log(findErrorNums([1, 1]))
+
+// 35 Search Insert Position
+
+var searchInsert = function (nums, target) {
+    let low = 0;
+    let high = nums.length;
+
+    while (low < high) {
+        let middleIndex = low + Math.floor((high - low) / 2);
+
+        if (nums[middleIndex] === target) {
+            return middleIndex;
+        }
+
+        if (target > nums[middleIndex]) {
+            low = middleIndex + 1;
+        } else {
+            high = middleIndex;
+        }
+    }
+    return low
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5))
+console.log(searchInsert([1, 3, 5, 6], 2))
+console.log(searchInsert([1, 3, 5, 6], 7))
