deac1ce7e9d1e6019a67a1c23160a9552cbc442d
trangeorge <trangeorge8@aol.com>
2023-06-28 19:31:43 -0500

    Updated 6/28

--- Leetcode/LeetCode_W5.js
+++ Leetcode/LeetCode_W5.js
@@ -40,3 +40,49 @@ var searchInsert = function (nums, target) {
 console.log(searchInsert([1, 3, 5, 6], 5))
 console.log(searchInsert([1, 3, 5, 6], 2))
 console.log(searchInsert([1, 3, 5, 6], 7))
+
+// 876 Middle of the Linked List
+
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+    }
+}
+
+var middleNode = function (head) {
+    let twice = head;
+    let once = head;
+    while (twice && twice.next) {
+        twice = twice.next.next;
+        once = once.next
+    }
+    return once
+};
+
+// console.log(middleNode([1, 2, 3, 4, 5]))
+
+// 520 Detect Capital
+
+const regexUppercase = /^[A-Z]+$/;
+const regexLowercase = /^[a-z]+$/;
+var detectCapitalUse = function (word) {
+    if (regexUppercase.test(word) || regexLowercase.test(word)) return true;
+    let output = false;
+
+    for (let i = 0; i < word.length; i++) {
+        if (regexUppercase.test(word[i]) && !i) output = true;
+        if (regexUppercase.test(word[i]) && i) output = false;
+    }
+
+    return output;
+};
+
+console.log(detectCapitalUse("USA"))
+console.log(detectCapitalUse("FLaG"))
