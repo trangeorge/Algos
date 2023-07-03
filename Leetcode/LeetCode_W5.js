caa62630f45e02bf63e771dbdc7d61a972fe06a4
trangeorge <trangeorge8@aol.com>
2023-07-02 19:32:57 -0500

    Updated 7/2

--- Leetcode/LeetCode_W5.js
+++ Leetcode/LeetCode_W5.js
@@ -161,3 +161,22 @@ const isPowerOfThree = n => {
 console.log(isPowerOfThree(27))
 console.log(isPowerOfThree(0))
 console.log(isPowerOfThree(-1))
+
+// 1 Two Sum
+
+var twoSum = function (nums, target) {
+
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+
+};
+
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 2, 4], 6))
+console.log(twoSum([3, 3], 6))
