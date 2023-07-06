42e7d7c7ab42b6a6c32c2d5d0437116828d4f424
trangeorge <trangeorge8@aol.com>
2023-07-05 20:10:11 -0500

    Updated 7/5

--- Leetcode/LeetCode_W6.js
+++ Leetcode/LeetCode_W6.js
@@ -33,6 +33,24 @@ console.log(romanToInt("III"))
 console.log(romanToInt("LVIII"))
 console.log(romanToInt("MCMXCIV"))
 
+//21 Merge Two Sorted Lists
+
+var mergeTwoLists = function (l1, l2) {
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+
+
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+
+    else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+};
+
 // 66 Plus One
 
 var plusOne = function (arr) {
