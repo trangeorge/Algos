156d90755054c5ca92bc3c36c20dbcb79b2cff4e
trangeorge <trangeorge8@aol.com>
2023-07-12 22:26:46 -0500

    Updated 7/12

--- Leetcode/LeetCode_W7.js
+++ Leetcode/LeetCode_W7.js
@@ -58,3 +58,44 @@ function isPowerOfFour(n) {
     }
     return n === 1;
 }
+
+// 344 Reverse String 
+
+var reverseString = function (s) {
+    let left = 0
+    let right = s.length - 1
+
+    while (left <= right) {
+        let temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        left++
+        right--
+    }
+
+    return s
+};
+
+// 345 Reverse Vowels of a String
+
+var reverseVowels = function (s) {
+    let left = 0
+    let right = s.length - 1
+    let arr = s.split("")
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', "I", 'O', 'U']
+
+    while (left < right) {
+        if (vowels.indexOf(arr[left]) !== -1 && vowels.indexOf(arr[right]) !== -1) {
+            [arr[left], arr[right]] = [arr[right], arr[left]]
+            ++left;
+            --right;
+        }
+        if (vowels.indexOf(arr[left]) == -1) {
+            ++left
+        }
+        if (vowels.indexOf(arr[right]) == -1) {
+            --right
+        }
+    }
+    return arr.join("")
+};
