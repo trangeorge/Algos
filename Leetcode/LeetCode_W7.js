477ef435bcda5d7d2368d292e2fe9463337dd3b0
trangeorge <trangeorge8@aol.com>
2023-07-16 21:35:09 -0500

    Updated 7/16

--- Leetcode/LeetCode_W7.js
+++ Leetcode/LeetCode_W7.js
@@ -206,3 +206,30 @@ var findLUSlength = function (a, b) {
     if (a.length > b.length) return a.length
     else return b.length
 };
+
+// 977 Squares of a Sorted Array
+
+const sortedSquares = function (nums) {
+    //create a new array the same length of the original array
+    const squaredArr = Array(nums.length)
+    let left = 0
+    let right = nums.length - 1
+    //iterate through the new array from the back
+    for (let i = nums.length - 1; i >= 0; i--) {
+        const leftSquared = nums[left] ** 2
+        const rightSquared = nums[right] ** 2
+        //if the left number squared is more than the right number squared
+        if (leftSquared > rightSquared) {
+            //set the current element to the left number squared and increment the left pointer
+            squaredArr[i] = leftSquared
+            left++
+        }
+        else {
+            //set the current element to the right number squared and increment the right pointer
+            squaredArr[i] = rightSquared
+            right--
+        }
+    }
+    //return the array
+    return squaredArr
+}
