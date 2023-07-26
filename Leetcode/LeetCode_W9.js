6756204b5972b0ab9b5ade6e96545c83062c1e3c
trangeorge <trangeorge8@aol.com>
2023-07-25 19:06:55 -0500

    Updated 7/25

--- Leetcode/LeetCode_W9.js
+++ Leetcode/LeetCode_W9.js
@@ -19,19 +19,15 @@
 
 
 var maximumProduct = function (nums) {
-    nums.sort((a, b) => a - b)
-
-    var lastNumber = nums.length - 1
-    var midNumber = nums.length - 2
-    var firstNumber = nums.length - 3
-    var total = nums[lastNumber] * nums[midNumber] * nums[firstNumber]
-    return total
-
+    let sorted = nums.sort((a, b) => a - b)
+    let res1 = sorted[0] * sorted[1] * sorted[nums.length - 1]
+    let res2 = sorted[nums.length - 1] * sorted[nums.length - 2] * sorted[nums.length - 3]
+    return Math.max(res1, res2);
 };
-
 console.log(maximumProduct([1, 2, 3]))
 console.log(maximumProduct([1, 2, 3, 4]))
 console.log(maximumProduct([-1, -2, -3]))
+console.log(maximumProduct([-100, -98, -1, 2, 3, 4]))
 
 
 
@@ -78,3 +74,43 @@ var findLengthOfLCIS = function (nums) {
 
 console.log(findLengthOfLCIS([1, 3, 5, 4, 7]))
 console.log(findLengthOfLCIS([2, 2, 2, 2, 2]))
+
+// 125 Valid Palindrome 
+
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+
+var isPalindrome = function (s) {
+    //^a-z0-9 represents that we must find all alphanumeric char and remove it.
+    //gi represents: g=finds all matches ; i=checks case sensitivity
+    //.toLowerCase() converts all uppercase letters to lowercase letters.
+    let newStr = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+
+    //if we reverse the string and it's equal to the newstr then we return true
+    //if not, then we return false.
+    return newStr.split("").reverse().join("") === newStr;
+};
+
