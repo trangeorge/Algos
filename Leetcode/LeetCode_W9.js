055bc14aa17d37165801aa8406e98d7ed7dda810
trangeorge <trangeorge8@aol.com>
2023-07-26 22:40:56 -0500

    Updated 7/26

--- Leetcode/LeetCode_W9.js
+++ Leetcode/LeetCode_W9.js
@@ -129,3 +129,41 @@ const validPalindrome = (s) => {
 const cut = (s, i) => s.substr(0, i) + s.substr(i + 1);
 
 const isPalindrome = (s) => s === s.split('').reverse().join('');
+
+
+//728 Seelf Dividing Numbers
+
+// A self-dividing number is a number that is divisible by every digit it contains.
+
+// For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+// A self-dividing number is not allowed to contain the digit zero.
+
+// Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right].
+
+
+
+// Example 1:
+
+// Input: left = 1, right = 22
+// Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+// Example 2:
+
+// Input: left = 47, right = 85
+// Output: [48,55,66,77]
+
+
+var selfDividingNumbers = function (left, right) {
+    let res = []
+
+    for (let i = left; i <= right; i++) {
+        let temp = i.toString().split('')
+        if (!temp.includes('0')) {
+            let pass = true
+            for (let j = 0; j < temp.length; j++) {
+                if (i % temp[j] !== 0) pass = false
+            }
+            if (pass) res.push(i)
+        }
+    }
+    return res
+};
