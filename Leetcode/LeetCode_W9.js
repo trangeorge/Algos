8576d7b9913366d5815a27d9b458b627e6689064
trangeorge <trangeorge8@aol.com>
2023-07-27 19:36:24 -0500

    Updated 7/27

--- Leetcode/LeetCode_W9.js
+++ Leetcode/LeetCode_W9.js
@@ -167,3 +167,38 @@ var selfDividingNumbers = function (left, right) {
     }
     return res
 };
+
+// 747. Largest Number At Least Twice of Others
+
+// You are given an integer array nums where the largest integer is unique.
+
+// Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
+
+
+// Example 1:
+
+// Input: nums = [3,6,1,0]
+// Output: 1
+// Explanation: 6 is the largest integer.
+// For every other number in the array x, 6 is at least twice as big as x.
+// The index of value 6 is 1, so we return 1.
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: -1
+// Explanation: 4 is less than twice the value of 3, so we return -1.
+
+var dominantIndex = function (nums) {
+    // Make a copy of the nums array to avoid modifying the original array
+    var sorted = [...nums].sort((a, b) => a - b);
+    var lastNum = sorted[nums.length - 1];
+    var secondNum = sorted[nums.length - 2];
+
+    if (lastNum >= secondNum * 2) {
+        return nums.indexOf(lastNum);
+    } else {
+        return -1;
+    }
+};
+console.log(dominantIndex([3, 6, 1, 0]));
+console.log(dominantIndex([1, 2, 3, 4]));
